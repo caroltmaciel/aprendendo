362d6940b5faadb6796da29c3816d4abf91ef296
Ana Carolina <anacarolinamaciel15@gmail.com>
2023-01-19 13:12:17 -0400

    noções de introdução, string, number e boolean em js

---
+++ js/number.js
@@ -0,0 +1,15 @@
+/*
+Number
+    * números
+
+    33 // inteiros
+    12.5 // reais - float
+    NaN // not a number
+    Infinity // infinito
+*/
+console.log(33)
+console.log(12.5)
+console.log(12.5 + 12)
+console.log(12.5 / "asfd") //not a bumber
+console.log(12.5 === Infinity) //acusa true or false
+console.log(Infinity) // não pode mudar o I para minúsculo pois dará um erro de referência
---
+++ js/strings.js
@@ -0,0 +1,14 @@
+/* 
+String
+
+    *Cadeia de caracteres
+
+"" // aspas duplas 
+'' // aspas simples
+`` // template literals ou template strings
+
+*/
+
+console.log('Ana Carolina')
+console.log('Ana Carolina "Ana Carolina"')
+console.log(`Ana Carolina ${1 + 1}`)
