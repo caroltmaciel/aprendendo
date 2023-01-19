8cfe096e852355ae42b368980987df4a7b16f854
Ana Carolina <anacarolinamaciel15@gmail.com>
2023-01-19 12:40:16 -0400

    noções de sintaxe e comentários em JS

---
+++ js/comentarios.js
@@ -0,0 +1,9 @@
+// Aqui eu tenho um comentário em linha
+
+/*
+    Aqui eu tenho um comentário multi linha
+    o código abaixo escre alguma mensagem no console
+*/
+
+
+console.log('Bem vindos ao Starter!!') // alguma coisa
